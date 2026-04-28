3d6ab6b563cd3c6c51692cc4637d07bc12a30226
Oscar87Wong <167697669+Oscar87Wong@users.noreply.github.com>
2026-04-28 23:24:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,13 +21,13 @@ from langchain_nvidia_ai_endpoints import ChatNVIDIA, NVIDIAEmbeddings
 
 
 # Access the API keys from secrets
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+#OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 NEWS_API_KEY = st.secrets["NEWS_API_KEY"]
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+#os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 #For NVidia AI API Keys
-#NVIDIA_API_KEY = st.secrets["NVIDIA_API_KEY"]
-#os.environ["NVIDIA_API_KEY"] = NVIDIA_API_KEY
+NVIDIA_API_KEY = st.secrets["NVIDIA_API_KEY"]
+os.environ["NVIDIA_API_KEY"] = NVIDIA_API_KEY
 
 # Page configuration
 st.set_page_config(
@@ -126,24 +126,24 @@ def build_rag_pipeline(articles):
         loader = TextLoader(temp_file, encoding="utf-8")
         documents = loader.load()
 
-        splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+        """splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
         docs = splitter.split_documents(documents)
 
         embeddings = OpenAIEmbeddings()
         db = FAISS.from_documents(docs, embeddings)
 
-        llm = OpenAI(temperature=0)
+        llm = OpenAI(temperature=0)"""
 
         #For NVidia API
-        #embeddings = NVIDIAEmbeddings(model="nvidia/nv-embedqa-e5-v5") 
+        embeddings = NVIDIAEmbeddings(model="nvidia/nv-embedqa-e5-v5") 
         
-        #db = FAISS.from_documents(docs, embeddings)
+        db = FAISS.from_documents(docs, embeddings)
 
         # --- CHANGE 2: Use NVIDIA Chat Model ---
         # You can choose models like "meta/llama-3.1-405b-instruct" or "nvidia/nemotron-4-340b-instruct"
-        #llm = ChatNVIDIA(model="meta/llama-3.1-8b-instruct", temperature=0.2)
+        llm = ChatNVIDIA(model="meta/llama-3.1-8b-instruct", temperature=0.2)
         
-        # 定義提示詞模板
+        
         prompt = ChatPromptTemplate.from_template("""
         Answer the following question based only on the provided financial news context:
         <context>
